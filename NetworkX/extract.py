db58c4b688674d5aec0b79234ba71029ddc5f68c
taylorodem <odem20@up.edu>
2020-09-09 13:35:59 -0700

    Update extract.py
    Combine extracting epsilon matrix values with NetworkX code

--- NetworkX/extract.py
+++ NetworkX/extract.py
@@ -1,3 +1,7 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
 def read_ep_matrix():
     ep_in = open("..\Main\output.txt")
 
@@ -16,7 +20,6 @@ def read_ep_matrix():
             #for x in spl:
                 #labels.append(int(x))
 
-            #print(labels)
         elif i != 0:
             row = line.split()
             row = row[1:]
@@ -27,5 +30,36 @@ def read_ep_matrix():
         i = i + 1
             
     return [labels, values]
+
+def main():
+    G = nx.Graph()
+    ep_arr = read_ep_matrix()
+    labels = ep_arr[0]
+    values = np.array(ep_arr[1])
+    
+    G = nx.convert_matrix.from_numpy_matrix(values,parallel_edges=False, create_using=None)
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
+
+#below function replaced by convert_matrix.from_numpy_matrix
+#will remove once positive above code works
+def dummy_double_loop():
+    row_num = 0
+    for row in values:
+        col_num = 0
+        for col in row:
+            i = 0
+            while i != col:
+                G.add_edge(labels[row_num], labels[col_num])
+                #print("adding edge to " + labels[row_num] + ", " + labels[col_num])
+                i = i + 1
+            col_num = col_num + 1
+        row_num = row_num + 1
+
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.show()
+                
+
+    
             
     
