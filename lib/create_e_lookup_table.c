e0291107d3ab4d061106c64e5e11b62f63a8bffd
TawnyMotoyama <motoyama20@up.edu>
2020-11-24 01:12:41 -1000

    added file and function headers and removed some commented code

--- lib/create_e_lookup_table.c
+++ lib/create_e_lookup_table.c
@@ -4,8 +4,40 @@
 #include <math.h>
 #include "create_e_lookup_table.h"
 
+/**
+ * This file contains functions that can be used to create the Epsilon Lookup Table. 
+ * This includes the functions that perform statistical comparison on rows and functionality 
+ * related to the statistical analysis (i.e. converting the C-matrix to probabilities). 
+ * The current stat tests implemented are chi-squared and g-test.
+ * 
+ * 
+ * 
+ */
+
+// Estimate of Euler's number
 #define EULERS 2.71828
 
+/**
+ * This function uses the C-matrix and a number of other factors (see parameter details for 
+ * more info on these factors) to create an Epsilon Lookup Table for the data. 
+ * 
+ * Note: The Epsilon Lookup Table is an array of "luRow" structs (where every "luRow" struct 
+ *          represents a row in the table). This table holds info entailing which C-matrix 
+ *          rows (behavioral primitives) are grouped together.
+ * 
+ * @param ptrToLookupTable: Pointer to the array of luRow structs to represent rows of the 
+ *                          lookup table (memory should be allocated to the correct size with 
+ *                          no real data in it yet)
+ * @param dir: Number of directions used in data collection (user-specified)
+ * @param realSize: Number of rows/columns in the C-matrix
+ * @param ptrToMatrix: Pointer to the C-matrix
+ * @param ptrToLabels: Pointer to the array of strings that are the labels for the C-matrix
+ * @param ALPHA: The grouping threshold specified by the user (normally ranges between .001-.1)
+ * @param STAT_METHOD: The statistical test specified by the user (1 if the user wants to 
+ *                      apply the chi-squared test and 2 if the user wants to apply the g-test)
+ * 
+ * @return: (int) Number of rows/columns of E-matrix
+ */
 int create_e_table(struct luRow **ptrToLookupTable, int dir, int realSize, double ***ptrToMatrix, char ***ptrToLabels, double ALPHA, int STAT_METHOD)
 {
     struct luRow *lookupTable = *ptrToLookupTable;
@@ -30,6 +62,26 @@ int create_e_table(struct luRow **ptrToLookupTable, int dir, int realSize, doubl
     return fillTab;
 }
 
+/**
+ * This function calls the necessary functions to perform statistical comparison on rows 
+ * and fills in the Epsilon Lookup Table with the correct information based on a full 
+ * statistical analysis of the C-matrix. This function loops through the C-matrix and 
+ * compares each row to every other row that has not already been grouped.
+ * 
+ * @param flags: An array of ints used to keep track of which rows have already been grouped 
+ *               (and therefore, do not need to be comapred again)
+ * @param ptrToLookupTable: Pointer to the array of luRow structs to represent rows of the 
+ *                          lookup table (memory should be allocated to the correct size with 
+ *                          no real data in it yet)
+ * @param dir: Number of directions used in data collection (user-specified)
+ * @param realSize: Number of rows/columns in the C-matrix
+ * @param ptrToMatrix: Pointer to the C-matrix
+ * @param ALPHA: The grouping threshold specified by the user (normally ranges between .001-.1)
+ * @param STAT_METHOD: The statistical test specified by the user (1 if the user wants to 
+ *                      apply the chi-squared test and 2 if the user wants to apply the g-test)
+ * 
+ * @return: (int) Number of rows/columns of E-matrix
+ */
 int fill_e_table(int *flags, struct luRow **ptrToLookupTable, int dir, int realSize, double ***ptrToMatrix, char ***ptrToLabels, double ALPHA, int STAT_METHOD)
 {
     struct luRow *lookupTable = *ptrToLookupTable;
@@ -56,7 +108,7 @@ int fill_e_table(int *flags, struct luRow **ptrToLookupTable, int dir, int realS
                     if (STAT_METHOD == 1)
                     {
                         //printf("Performing X squared test\n");
-                        compare_rows(matrix[currentLead], matrix[i], realSize, &total);
+                        chi_squared_test(matrix[currentLead], matrix[i], realSize, &total);
                     }
                     if (STAT_METHOD == 2)
                     {
@@ -100,6 +152,16 @@ int fill_e_table(int *flags, struct luRow **ptrToLookupTable, int dir, int realS
     return uniqueEls;
 }
 
+/**
+ * This function performs a g-test on the two rows passed as parameters.
+ * 
+ * @param leadRow: The "lead" row to compare the current row (row that has not been 
+ *                  grouped yet) against
+ * @param otherRow: The current row (row that has not been grouped yet)
+ * @param realSize: Number of rows/columns in the C-matrix
+ * @param total: The g-test value (if this value is less than ALPHA, then the "otherRow" 
+ *                  can be considered statistically similar to the "leadRow")
+ */
 int g_test(double *leadRow, double *otherRow, int realSize, double *total)
 {
     double currentTotal = 0.0;
@@ -120,7 +182,17 @@ int g_test(double *leadRow, double *otherRow, int realSize, double *total)
     return 1;
 }
 
-int compare_rows(double *row1, double *row2, int realSize, double *total)
+/**
+ * This function performs a chi-squared test on the two rows passed as parameters.
+ * 
+ * @param row1: The "lead" row to compare the current row (row that has not been 
+ *                  grouped yet) against
+ * @param row2: The current row (row that has not been grouped yet)
+ * @param realSize: Number of rows/columns in the C-matrix
+ * @param total: The g-test value (if this value is less than ALPHA, then the "otherRow" 
+ *                  can be considered statistically similar to the "leadRow")
+ */
+int chi_squared_test(double *row1, double *row2, int realSize, double *total)
 {
     double sum1 = 0;
     double sum2 = 0;
@@ -128,7 +200,6 @@ int compare_rows(double *row1, double *row2, int realSize, double *total)
     int degreeOfFreedom = 0;
     for (int i = 0; i < realSize; i++)
     {
-        //printf("%f : %f\n", row1[i], row2[i]);
         sum1 += row1[i];
         sum2 += row2[i];
         if ((row1[i] > 0) || (row2[i] > 0))
@@ -136,9 +207,6 @@ int compare_rows(double *row1, double *row2, int realSize, double *total)
             degreeOfFreedom++;
         }
     }
-    //printf("DEGREE OF FREEDOM: %d\n", degreeOfFreedom);
-    // printf("sum1: %f\n", sum1);
-    // printf("sum2: %f\n", sum2);
 
     double k1 = 0;
     if (sum1 != 0)
@@ -150,33 +218,17 @@ int compare_rows(double *row1, double *row2, int realSize, double *total)
     {
         k2 = sqrt(sum1 / sum2);
     }
-    // printf("k1: %f\n", k1);
-    // printf("k2: %f\n", k2);
 
     for (int k = 0; k < realSize; k++)
     {
-        // printf("row1[k]: %f\n", row1[k]);
-        // printf("row2[k]: %f\n", row2[k]);
         double denominator = row1[k] + row2[k];
         if (denominator != 0)
         {
-            // ********* pow(x, y) = x raised to the power of y
             double numerator = pow((k1 * row1[k] - k2 * row2[k]), 2.0);
 
-            // printf("k1 * row1[k]: %f\n", k1 * row1[k]);
-            // printf("k2 * row2[k]: %f\n", k2 * row2[k]);
-            // printf("numerator: %f\n", numerator);
-            // printf("denominator: %f\n", denominator);
-
-            //*total += chsppf((numerator / denominator), realSize);
             *total += (numerator / denominator);
-
-            // printf("Total: %f\n", *total);
-            // printf("\n\n");
         }
     }
-
-    //printf("Total: %f\n", *total);
 
     double adjustedChiSquared = 0.0;
     if (*total > 0.0)
@@ -190,20 +242,30 @@ int compare_rows(double *row1, double *row2, int realSize, double *total)
     return 1;
 }
 
-double chsppf(double chiSquaredVal, int rowLength)
+/**
+ * This function applies the chi-squared percent point function to the chi-squared value 
+ * calculated in the chi_squared_test function. This function was needed to account for 
+ * the degrees of freedom (in this case in all non-zero bins in a row)
+ * 
+ * @param chiSquaredVal: The chi-squared value calculated in the chi_squared_test function
+ * @param degreeOfFreedom: Degree of freedom value (all non-zero bins in a row))
+ * 
+ * @return: (double) Adjusted chi-squared value
+ */
+double chsppf(double chiSquaredVal, int degreeOfFreedom)
 {
     double eulersVal = pow(EULERS, (0 - chiSquaredVal) / 2);
-    double xVal = pow(chiSquaredVal, (rowLength / 2) - 1);
+    double xVal = pow(chiSquaredVal, (degreeOfFreedom / 2) - 1);
     double numerator = eulersVal * xVal;
 
-    int x = (rowLength / 2) - 1;
+    int x = (degreeOfFreedom / 2) - 1;
     double gammaVal = 1;
     for (int i = 1; i <= x; i++)
     {
         gammaVal = gammaVal * i;
     }
 
-    double twosVal = pow(2.0, (rowLength / 2));
+    double twosVal = pow(2.0, (degreeOfFreedom / 2));
     double denominator = twosVal * gammaVal;
 
     double alpha = numerator / denominator;
@@ -211,6 +273,13 @@ double chsppf(double chiSquaredVal, int rowLength)
     return alpha;
 }
 
+/**
+ * This function converts the C-matrix values to probabilities. This prepares the matrix 
+ * for statistical analysis.
+ * 
+ * @param realSize: Number of rows/columns in the C-matrix
+ * @param ptrToMatrix: Pointer to the C-matrix
+ */
 int convert_to_probabilities(int realSize, double ***ptrToMatrix)
 {
     double **matrix = *ptrToMatrix;
@@ -224,15 +293,12 @@ int convert_to_probabilities(int realSize, double ***ptrToMatrix)
         }
     }
 
-    //matrixSum = 146;
     printf("Matrix Sum = %f\n", matrixSum);
-
     for (i = 0; i < realSize; i++)
     {
         for (j = 0; j < realSize; j++)
         {
             matrix[i][j] = matrix[i][j] / matrixSum;
-            //printf("[%d, %d] = %f\n", i, j, matrix[i][j]);
         }
     }
     return 1;
--- lib/create_e_matrix.c
+++ lib/create_e_matrix.c
@@ -4,6 +4,32 @@
 #include "read_c_vectors.h"
 #include "create_e_matrix.h"
 
+/**
+ * This file contains a function that creates the Epsilon Matrix (E-matrix). The E-matrix is 
+ * created by referring back to the original input data and using the Epsilon Lookup Table to 
+ * map each vector to a spot in the E-matrix. The E-matrix is written to an output file 
+ * specified by the user.
+ * 
+ * 
+ * 
+ */
+
+/**
+ * This function creates the E-matrix and writes it to the output file specified by the user.
+ * 
+ * @param ptrToLookupTable: Pointer to the array of luRow structs to represent rows of the 
+ *                          lookup table 
+ * @param sizeofLookupTable: Number of rows in the Epsilon Lookup Table (this is equivalent to 
+ *                              the number of rows/columns in the C-matrix)
+ * @param numRows: Number of rows in the E-matrix
+ * @param ptrToEMat: Pointer to the E-matrix
+ *                   (memory should be allocated to the correct size, and should only 
+ *                    be initialized to zeros--no real data in it yet)
+ * @param input_file: The input file specified by user
+ * @param output_file: The output file specified by user
+ * @param dir: Number of directions used in data collection (user-specified)
+ * @param hist: Number of integers to be used in a single vector (user-specified)
+ */
 int create_e_matrix(struct luRow **ptrToLookupTable, int sizeofLookupTable, int numRows, int ***ptrToEMat, FILE *input_file, FILE *output_file, int dir, int hist)
 {
     struct luRow *lookupTable = *ptrToLookupTable;
@@ -26,18 +52,12 @@ int create_e_matrix(struct luRow **ptrToLookupTable, int sizeofLookupTable, int 
     {
         cvec[i] = 0;
     }
-    //printf("%s -> ", vector);
     // Get compressed vec
     for (i = 0; i < hist; i++)
     {
         int val = vector[i] - '0';
         cvec[val - 1] += 1;
     }
-    // for (i = 0; i < dir; i++)
-    // {
-    //     printf("%d", cvec[i]);
-    // }
-    //printf("\n");
     for (i = 0; i < dir; i++)
         current[i] = cvec[i] + '0';
     current[dir] = '\0';
@@ -48,18 +68,12 @@ int create_e_matrix(struct luRow **ptrToLookupTable, int sizeofLookupTable, int 
         {
             cvec[i] = 0;
         }
-        //printf("%s -> ", vector);
         // Get compressed vec
         for (i = 0; i < hist; i++)
         {
             int val = vector[i] - '0';
             cvec[val - 1] += 1;
         }
-        // for (i = 0; i < dir; i++)
-        // {
-        //     printf("%d", cvec[i]);
-        // }
-        // printf("\n");
         for (i = 0; i < dir; i++)
             next[i] = cvec[i] + '0';
         next[dir] = '\0';
@@ -69,7 +83,6 @@ int create_e_matrix(struct luRow **ptrToLookupTable, int sizeofLookupTable, int 
             if (strcmp(current, lookupTable[i].vector) == 0)
             {
                 currentPos = lookupTable[i].epsilonIdx;
-                //printf("currentPos: %d\n", currentPos);
                 break;
             }
         }
@@ -79,7 +92,6 @@ int create_e_matrix(struct luRow **ptrToLookupTable, int sizeofLookupTable, int 
             if (strcmp(next, lookupTable[j].vector) == 0)
             {
                 nextPos = lookupTable[j].epsilonIdx;
-                //printf("nextPos: %d\n", nextPos);
                 break;
             }
         }
